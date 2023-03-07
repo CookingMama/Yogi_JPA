f74f1fd67cb05f676189f8665d0c23d1f1f2cb74
youruchul <youruchul@naver.com>
2023-03-07 10:13:10 +0900

    init

---
+++ src/main/java/com/papa/yogiyogi/YogiyogiApplication.java
@@ -0,0 +1,13 @@
+package com.papa.yogiyogi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YogiyogiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YogiyogiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/papa/yogiyogi/YogiyogiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.papa.yogiyogi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YogiyogiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
